57e982140356d7fd8898fa5b724ea6686985fc67
Omar Hussein <omarelwy@hotmail.com>
2021-12-29 04:30:05 +0200

    🔥 Remove all the cursor stuff

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,32 +1,3 @@
-<script setup lang="ts">
-import { emitCursorActivateEvents } from "~~/utils";
-
-const layout = ref(null as null | HTMLElement);
-
-let destroy = () => {};
-
-// onMounted(init);
-// onUnmounted(destroy);
-
-function init() {
-  const { destroy: destroyEvents } = emitCursorActivateEvents(
-    layout.value as HTMLElement
-  );
-  destroy = destroyEvents;
-}
-</script>
-
 <template>
-  <div ref="layout">
-    <slot></slot>
-    <!-- <Cursor /> -->
-  </div>
+  <slot></slot>
 </template>
-
-<style lang="scss">
-// *,
-// *::after,
-// *::before {
-//   cursor: none;
-// }
-</style>
