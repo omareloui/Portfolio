6ce74443c95288ad505f48736c49391aa1b96c42
Omar Hussein <omarelwy@hotmail.com>
2022-01-11 06:30:09 +0200

    ✨ Create a heading component

---
+++ components/Heading.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const { level } = defineProps<{ level: string }>();
+</script>
+
+<template>
+  <component :is="level" class="heading"><slot></slot></component>
+</template>
+
+<style lang="scss" scoped>
+@use "~~/assets/styles/mixins" as *;
+
+.heading {
+  @include st-tablet {
+    text-align: center;
+  }
+}
+</style>
