54ae2b605eadec9c4429004606703444bd1f3114
Omar Hussein <omarelwy@hotmail.com>
2022-01-11 06:30:37 +0200

    🎨 Update the section container to be able to define the gap and remove margins

--- components/SectionContainer.vue
+++ components/SectionContainer.vue
@@ -1,17 +1,39 @@
+<script setup lang="ts">
+const { gap, noMarginBottom, noMarginTop } = withDefaults(
+  defineProps<{
+    noMarginTop?: boolean;
+    noMarginBottom?: boolean;
+    gap?: string;
+  }>(),
+  { noMarginBottom: false, noMarginTop: false, gap: "30px" }
+);
+</script>
+
 <template>
-  <Container tag="section" class="section-container">
+  <Container
+    tag="section"
+    class="section-container"
+    :class="{
+      'section-container--has-margin-top': !noMarginTop,
+      'section-container--has-margin-bottom': !noMarginBottom,
+    }"
+  >
     <slot></slot>
   </Container>
 </template>
 
 <style lang="scss" scoped>
 .section-container {
-  --v-space: 30px;
+  --v-space: v-bind(gap);
 
-  margin-top: var(--v-space);
-  margin-bottom: var(--v-space);
+  &--has-margin-top {
+    margin-top: var(--v-space);
+    padding-top: 0.1px;
+  }
 
-  padding-top: 0.1px;
-  padding-bottom: 0.1px;
+  &--has-margin-bottom {
+    margin-bottom: var(--v-space);
+    padding-bottom: 0.1px;
+  }
 }
 </style>
