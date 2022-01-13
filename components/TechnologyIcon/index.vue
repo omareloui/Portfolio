60a661f99d14608908675ea30d6677bfc9435b32
Omar Hussein <omarelwy@hotmail.com>
2022-01-13 04:29:01 +0200

    🎨 Add the ability to add text to the technology icon

--- components/TechnologyIcon/index.vue
+++ components/TechnologyIcon/index.vue
@@ -1,24 +1,56 @@
 <script setup lang="ts">
-const { title } = defineProps<{ title: string }>();
+const { title, showTitle } = withDefaults(
+  defineProps<{ title: string; showTitle?: boolean }>(),
+  {
+    showTitle: false,
+  }
+);
 </script>
 
 <template>
-  <div class="technology-icon" v-bind="{ title }">
-    <slot></slot>
+  <div class="technology">
+    <div class="technology__icon" v-bind="{ title }">
+      <slot></slot>
+    </div>
+
+    <span class="technology__title" v-if="showTitle">
+      {{ title }}
+    </span>
   </div>
 </template>
 
 <style lang="scss" scoped>
 @use "~~/assets/styles/mixins" as *;
 
-.technology-icon {
-  @include size(60px);
+.technology {
+  --icon-size: 60px;
+
   background: var(--clr-light);
   padding: 0.5rem;
   border-radius: 5px;
 
-  ::v-deep(svg) {
-    @include size(100%);
+  &__icon {
+    @include size(var(--icon-size));
+
+    ::v-deep(svg) {
+      @include size(100%);
+    }
+  }
+
+  &__title {
+    display: inline-block;
+
+    text-align: center;
+    max-width: var(--icon-size);
+    width: 100%;
+    margin-top: 5px;
+
+    font-family: var(--fnt-main);
+    font-size: var(--fnt-size-small);
+    font-weight: var(--fnt-weight-regular);
+
+    text-overflow: ellipsis;
+    overflow: hidden;
   }
 }
 </style>
