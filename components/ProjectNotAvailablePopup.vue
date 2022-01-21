a1e68bfef4fb16284d70c7989e3255b1e115f1a1
Omar Hussein <omarelwy@hotmail.com>
2022-01-21 14:49:06 +0200

    🎨 Close the popup on clicking away

--- components/ProjectNotAvailablePopup.vue
+++ components/ProjectNotAvailablePopup.vue
@@ -1,14 +1,33 @@
 <script setup lang="ts">
 const emit = defineEmits(["close"]);
+const body = ref(null as null | HTMLElement);
 
 function close() {
   emit("close");
+}
+
+onMounted(init);
+onUnmounted(destroy);
+
+function init() {
+  addEventListener("pointerdown", onClick);
+}
+
+function onClick(e: PointerEvent) {
+  if (!body.value) return;
+
+  const hitBody = e.composedPath().includes(body.value);
+  if (!hitBody) close();
+}
+
+function destroy() {
+  removeEventListener("pointerdown", onClick);
 }
 </script>
 
 <template>
   <div class="project-not-available">
-    <div class="project-not-available__body">
+    <div class="project-not-available__body" ref="body">
       <h2 class="heading">The link isn't available yet</h2>
       This project isn't live yet. The link will be provided soon enough.
       <button class="close-button" @click="close">
@@ -31,7 +50,13 @@ function close() {
 
   color: var(--clr-text-light);
   background: var(--clr-primary-gradient-opacified);
-  backdrop-filter: blur(5px);
+  backdrop-filter: blur(12px);
+
+  font-size: 1.2rem;
+
+  @include st-mobile {
+    font-size: 1rem;
+  }
 
   &__body {
     padding: 2rem;
@@ -48,9 +73,14 @@ function close() {
 
   .heading {
     font-size: 2rem;
+
+    @include st-mobile {
+      font-size: 1.4rem;
+    }
   }
 
   .close-button {
+    cursor: pointer;
     border: none;
     border-radius: 999px;
     background: var(--clr-text-light);
@@ -61,7 +91,6 @@ function close() {
     font-size: var(--fnt-size-link-normal);
     @include text-bold;
 
-    cursor: pointer;
     &__content {
       @include text-gradient;
     }
