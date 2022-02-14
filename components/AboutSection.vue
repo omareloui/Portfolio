b2fff2190098051e9ff479360c34bfcb6cfc9e13
Omar Hussein <omarelwy@hotmail.com>
2022-02-14 21:47:13 +0200

    ✏️ Add more to the about me section

--- components/AboutSection.vue
+++ components/AboutSection.vue
@@ -9,19 +9,19 @@
       <article>
         <Paragraph padding="0.1rem 0">
           Hello world! I'm Omar. I'm a full-stack web developer based in Egypt.
-          <!-- </Paragraph>
+        </Paragraph>
 
-        <Paragraph padding="0.1rem 0"> -->
+        <Paragraph padding="0.1rem 0">
           My interest in web development started back in 2017, when I started to
           build scripts and applications to help me automate tasks I do
           regularly or solve problems in my day-to-day life. Since then and I
           started sharpening my skills to start working as a freelancer.
         </Paragraph>
 
-        <!-- <Paragraph padding="0.1rem 0">
+        <Paragraph padding="0.1rem 0">
           I'm excited to make the leap and continue refining my skills with the
           right company.
-        </Paragraph> -->
+        </Paragraph>
 
         <Paragraph>
           Other than developing I enjoy leather work, you can check my leather
