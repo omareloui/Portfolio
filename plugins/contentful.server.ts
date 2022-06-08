64c0ed29ec202bffdd21d8523af224db4ddd8be6
Omar Hussein <omarelwy@hotmail.com>
2022-06-08 06:21:42 +0200

    🐛 Make contentful plugin work on build

--- plugins/contentful.server.ts
+++ plugins/contentful.server.ts
@@ -1,9 +1,8 @@
-import { createClient } from "contentful";
+import contentful from "contentful";
 
 export default defineNuxtPlugin(() => {
   const config = useRuntimeConfig();
-
-  const client = createClient({
+  const client = contentful.createClient({
     space: config.private.CONTENTFUL_SPACE_ID,
     accessToken: config.private.CONTENTFUL_ACCESS_TOKEN,
   });
