6ba4a8b9a0dec9a4623342abd4eae2f88ddc69c8
Omar Eloui <omareloui@hotmail.com>
2023-01-16 00:46:58 +0200

    feat(404): add 404 page

---
+++ src/pages/quotes/random.ts
@@ -0,0 +1,47 @@
+import fs from "node:fs/promises";
+import { random } from "@utils";
+
+import type { APIRoute } from "astro";
+
+import type { Quote } from "@type";
+
+function sleep(ms: number) {
+  return new Promise(res => setTimeout(res, ms));
+}
+
+export const get: APIRoute = async function get() {
+  let quotes: Quote[] = [];
+
+  try {
+    await fs.access("./public/quotes.json", fs.constants.F_OK);
+    await read();
+  } catch (e) {
+    await refetch();
+  }
+
+  async function refetch() {
+    const _quotes = await (await fetch("https://type.fit/api/quotes")).json();
+    await fs.writeFile(
+      "./public/quotes.json",
+      JSON.stringify({ quotes: _quotes }, null, 2)
+    );
+    quotes = _quotes;
+  }
+
+  async function read() {
+    const quotesString = await fs.readFile("./public/quotes.json", {
+      encoding: "utf8",
+    });
+
+    quotes = JSON.parse(quotesString).quotes;
+  }
+
+  const randomQuote = quotes[random(0, quotes.length)];
+
+  return new Response(JSON.stringify(randomQuote, null, 2), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
