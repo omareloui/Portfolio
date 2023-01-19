f09178b03f6b3d6548da99fe75427fb65a907704
Omar Eloui <omareloui@hotmail.com>
2023-01-19 23:07:21 +0200

    feat(gtag): add Google Analytics (with partytown)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,18 @@ import { defineConfig } from "astro/config";
 import glsl from "vite-plugin-glsl";
 
 // https://astro.build/config
+import partytown from "@astrojs/partytown";
+
+// https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [glsl()],
   },
+  integrations: [
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
