74407f309887aaf4cf66ff855379c4cb47b938d2
Omar Eloui <contact@omareloui.com>
2026-04-23 22:47:03 +0200

    feat(resume): calculate the cv on client and make `/resume` download the resume

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,20 @@ import expressiveCode from "astro-expressive-code";
 import { defineConfig } from "astro/config";
 import glsl from "vite-plugin-glsl";
 
+const RESUME_URL = "https://docs.google.com/document/d/1bnDG2nmA_nEA9bUAqrBCYPAzzlWZh_vcDr4ts7ZApQo/export?format=pdf";
+
 export default defineConfig({
   site: "https://omareloui.com",
+
+  redirects: {
+    "/resume": { status: 302, destination: RESUME_URL },
+    "/cv": { status: 302, destination: RESUME_URL },
+  },
+
   vite: {
     plugins: [glsl()],
   },
+
   integrations: [
     partytown({ config: { forward: ["dataLayer.push"] } }),
     expressiveCode({ themes: ["github-dark"] }),
@@ -17,4 +26,3 @@ export default defineConfig({
     sitemap(),
   ],
 });
-
